885b7972d0823a8722deb0586723a50e23b26386
Tamir Haim Rabia <tamir.rabia@gmail.com>
2017-12-15 03:16:29 +0200

    ..

--- components/MainMap.js
+++ components/MainMap.js
@@ -11,6 +11,7 @@ import {
 import MapView from 'react-native-maps';
 
 const {height, width} = Dimensions.get('window');
+const FIRST_LEVEL_HEIGHT = 80;
 
 class MainMap extends Component {
 
@@ -21,8 +22,12 @@ class MainMap extends Component {
             items: [{name: 'אני עובד'}, {name: 'אני עובד'}],
             scrollEnabled: true,
             layoutHeight: 0,
-            spContainerTranslateY: []
+            spContainerTranslateY: [],
+            spContainerScaleX: [],
+            spContainerOpacity: []
         }
+
+        this.spContainerScaleXValue = [];
     }
 
     componentWillMount() {
@@ -36,7 +41,7 @@ class MainMap extends Component {
                     return false
                 },
                 onMoveShouldSetPanResponder: (event, gestureState) => {
-                    return gestureState.dy < -1
+                    return gestureState.dy < - 10
                 },
                 onMoveShouldSetPanResponderCapture: (event, gestureState) => {
                     return false;
@@ -54,63 +59,124 @@ class MainMap extends Component {
             })
         }
 
-        let animatedTranslateX = [];
+        let animatedTranslateY = [];
+        let animatedScaleX = [];
+        let animatedOpacity = [];
         for (let i = 0; this.state.items.length > i; i++) {
-            animatedTranslateX.push(new Animated.Value(height));
+            animatedTranslateY.push(new Animated.Value(height));
+            animatedScaleX.push(new Animated.Value(0.9));
+            animatedOpacity.push(new Animated.Value(1))
+
         }
 
-        this.setState({spContainerTranslateY: animatedTranslateX});
+        this.setState({
+            spContainerTranslateY: animatedTranslateY,
+            spContainerScaleX: animatedScaleX,
+            spContainerOpacity: animatedOpacity
+        }, () => {
+            for (let i = 0; this.state.items.length > i; i++) {
+                this.spContainerScaleXValue.push(animatedScaleX[i].interpolate({
+                    inputRange: [0, 0.9, 1],
+                    outputRange: [0.9, 0.9, 1]
+                }));
+            }
+        });
     }
 
 
     onStart(e, g, index) {
         this.setState({scrollEnabled: false});
-        const {locationY} = e.nativeEvent;
-        this.startingPoint = locationY + 30;
+        // this.scrollViewRef.scrollTo({
+        //     x: width * index,
+        //     y: 0,
+        //     animation: true
+        // })
     }
 
     onMove(e, g, index) {
 
         const {dy} = g;
         const {locationY} = e.nativeEvent;
-        console.log(dy);
 
-        const {spContainerTranslateY, layoutHeight} = this.state;
-        Animated.timing(spContainerTranslateY[index], {
-            duration: 0,
-            toValue: (layoutHeight - 30  ) + dy,
-            useNativeDriver: true
-        }).start();
+        const {spContainerTranslateY, spContainerScaleX, layoutHeight, spContainerOpacity} = this.state;
+
+        const newPosition = (layoutHeight - FIRST_LEVEL_HEIGHT) + dy;
+
+        const present = (1 - (newPosition / layoutHeight));
+
+        let opacityArrayAnim = [];
+        spContainerOpacity.map((item, i) => {
+            opacityArrayAnim.push(Animated.timing(item, {
+                duration: 0,
+                toValue: index === i ? 1 : (1 - present),
+                useNativeDriver: true
+            }))
+
+        });
+
+        Animated.parallel([
+            ...opacityArrayAnim,
+            Animated.timing(spContainerTranslateY[index], {
+                duration: 0,
+                toValue: newPosition < 0 ? 0 : newPosition,
+                useNativeDriver: true
+            }),
+            Animated.timing(spContainerScaleX[index], {
+                duration: 0,
+                toValue: present > 1 ? 1 : present,
+                useNativeDriver: true
+            })
+        ]).start();
+
     }
 
     onEnd(e, g, index) {
 
-        const {spContainerTranslateY, layoutHeight} = this.state;
+        const {spContainerTranslateY, layoutHeight, spContainerScaleX, spContainerOpacity} = this.state;
+        const duration = 200;
+        let opacityArrayAnim = [];
+        spContainerOpacity.map((item, i) => {
+            opacityArrayAnim.push(Animated.timing(item, {
+                duration,
+                toValue: 1,
+                useNativeDriver: true
+            }))
 
-        Animated.timing(spContainerTranslateY[index], {
-            duration: 100,
-            toValue: (layoutHeight - 30),
-            useNativeDriver: true,
-            easing: Easing.out(Easing.cubic)
-        }).start(() => {});
+        });
+        Animated.parallel([
+            ...opacityArrayAnim,
+            Animated.timing(spContainerTranslateY[index], {
+                duration,
+                toValue: (layoutHeight - FIRST_LEVEL_HEIGHT),
+                useNativeDriver: true,
+                easing: Easing.out(Easing.cubic)
+            }),
+            Animated.timing(spContainerScaleX[index], {
+                duration,
+                toValue: 0,
+                useNativeDriver: true
+            })]).start(() => {
+        });
         this.setState({scrollEnabled: true});
     }
 
     _renderSp = () => {
 
-        const {spContainerTranslateY} = this.state;
+        const {spContainerTranslateY, spContainerOpacity} = this.state;
         return this.state.items.map((item, index) => {
             return (<Animated.View key={index} {...this._panResponder(index).panHandlers}
                                    style={{
-                                       transform: [{translateY: spContainerTranslateY[index]}],
-                                       width: width - 50,
+                                       transform: [
+                                           {translateY: spContainerTranslateY[index]},
+                                           {scale: this.spContainerScaleXValue.length > 0 ? this.spContainerScaleXValue[index] : 0},
+                                       ],
+                                       opacity: spContainerOpacity[index],
+                                       width: width,
                                        height: this.state.layoutHeight,
                                        backgroundColor: 'red',
-                                       marginHorizontal: 10
                                    }}>
 
                 <Text>{item.name}</Text>
-
             </Animated.View>)
         });
     };
@@ -127,7 +193,7 @@ class MainMap extends Component {
                         this.state.spContainerTranslateY.map((item) => {
                             animationParalel.push(Animated.timing(item, {
                                 duration: 100,
-                                toValue: height - 30,
+                                toValue: height - FIRST_LEVEL_HEIGHT,
                                 easing: Easing.out(Easing.cubic),
                                 useNativeDriver: true
                             }))
@@ -149,7 +215,9 @@ class MainMap extends Component {
                     }}
                 />
 
-                <ScrollView scrollEnabled={this.state.scrollEnabled} horizontal showsHorizontalScrollIndicator={false}
+                <ScrollView ref={(ref) => {
+                    this.scrollViewRef = ref;
+                }} scrollEnabled={this.state.scrollEnabled} horizontal showsHorizontalScrollIndicator={false}
                             style={{position: 'absolute', bottom: 0, left: 0, right: 0}}>
                     <View style={{flexDirection: 'row'}}>
                         {this._renderSp()}
